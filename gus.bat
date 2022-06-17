dd57242f16b267f562b24d120f615d5820882c97
ThirtySomething <developer@derpaul.net>
2022-06-17 13:19:53 +0200

    Some minor fixed

---
+++ gus.bat
@@ -0,0 +1,5 @@
+@rem ***************************************************************************
+@rem Script to update all submodules to latest
+@rem ***************************************************************************
+@echo off
+git submodule update --recursive --remote
